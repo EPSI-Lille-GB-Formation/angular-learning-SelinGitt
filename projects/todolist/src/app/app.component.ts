e5a19622a7ec5921282ea9f04a1f8bad4887296a
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-12-08 10:09:56 +0100

    update todolist

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from '../models/todo';
+import { TODOS } from '../mock-todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'My Todo List';
+
+  todolist: Todo[] = TODOS;
+
+  constructor() { }
+
+  ngOnInit() {
+  console.table(this.todolist);
+  this.selectedTodo(8);
+ }
+
+ selectedTodo(id: number) {
+    console.table(this.todolist[id-1]);
+  }
 }
